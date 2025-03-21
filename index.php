8ac3eb8877c88de7ed8fc5519c27670f2bc5d651
MuriloCavalcanteLima <muricav@hotmail.com>
2025-03-21 00:53:38 -0300

    add config file for api key and index

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    session_start();
+
+    if (isset($_SESSION['flash_message'])) {
+        $flash = $_SESSION['flash_message'];
+        echo '<div class="' . htmlspecialchars($flash['type']) . '">' . htmlspecialchars($flash['message']) . '</div>';
+        unset($_SESSION['flash_message']);
+    }
+
+	require_once __DIR__ . '/controllers/matches_controller.php';
+
+	$controller = new MatchesController();
+	$controller->index();
+?>
