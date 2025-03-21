5e9cf52261cd0a149b1f85776e971263cf9e5398
MuriloCavalcanteLima <muricav@hotmail.com>
2025-03-21 00:51:23 -0300

    add model for matches

---
+++ models/MatchData.php
@@ -0,0 +1,55 @@
+<?php
+    class MatchData {
+        private $apiToken =  '56addeef3f3047658970676611b21143';
+        
+        public function getData($url) {
+            $this->url = $url;
+
+            $data = $this->request_data($this->url);
+
+            return json_decode($data, true);
+        }
+
+        public function getCompetitions() {
+            $data = $this->request_data('https://api.football-data.org/v4/competitions');
+            return json_decode($data, true);
+        }
+
+        public function getTeams($competition) {
+            $data = $this->request_data("https://api.football-data.org/v4/competitions/{$competition}/teams");
+            return json_decode($data, true);
+        }
+
+        public function getAllTeams() {
+            $data = $this->request_data("https://api.football-data.org/v4/teams");
+            return json_decode($data, true);
+        }
+
+        public function getCompetitionIdBySlug($slug) {
+            $competitions = $this->getCompetitions();
+            foreach($competitions['competitions'] as $competition) {
+                if ($competition['code'] == $slug) {
+                    return $competition['id'];
+                }
+            }
+            return null;
+        }
+
+        public function getCompetitionSlugs() {
+            $slugs = [];
+            $competitions = $this->getCompetitions();
+            foreach($competitions['competitions'] as $competition) {
+                $slugs[] = $competition['code'];
+            }
+            return $slugs;
+        }
+
+        private function request_data($url) {
+            $reqPrefs['http']['method'] = 'GET';
+            $reqPrefs['http']['header'] = "X-Auth-Token: $this->apiToken";
+            $stream_context = stream_context_create($reqPrefs);
+            $response = file_get_contents($url, false, $stream_context);
+            return $response;
+        }
+    }
+?>
